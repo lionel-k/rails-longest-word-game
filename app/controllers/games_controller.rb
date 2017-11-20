aad4f7c8afbd2328ed5d1a52ee47fd5956f2425f
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-20 14:27:07 +0100

    computing score done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,33 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = ("A".."Z").to_a.sample(10)
   end
 
   def score
-    binding.pry
+    @grid = params[:letters]
+    @guess = params[:guess].upcase
+    if guess_in_grid
+      url = "https://wagon-dictionary.herokuapp.com/#{@guess}"
+      result = JSON.parse(open(url).read)
+      if result["found"]
+        @message = "Congrats. #{@guess} is a valid word."
+      else
+        @message = "Sorry but #{@guess} does not seem to be a valid English word."
+      end
+    else
+      @message = "Sorry. #{@guess} can not be built out of #{@grid.split('').join(", ")}."
+    end
+  end
+
+  private
+
+  def guess_in_grid
+    @guess.split.all? do |l|
+      @grid.count(l) >= 1 && @guess.count(l) <= @grid.count(l)
+    end
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'new', to: "games#new", as: :new
-  get 'score', to: "games#score", as: :score
+  post 'score', to: "games#score", as: :score
 end
 
 
